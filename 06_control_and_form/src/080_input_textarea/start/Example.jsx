b85394d134a028f33774054b8475e41bd411e610
holy <68938511+holys519@users.noreply.github.com>
2024-09-22 21:17:19 +0900

    2024/09/22

--- 06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ 06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] =useState("");
+  const clearVal = () => setVal("");
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <div>
+        <input
+        id="123"
+        placeholder="こんにちは"
+        value={val}
+        onChange={(e) => setVal(e.target.value)} />
+        <textarea
+        id="345"
+        placeholder="こんにちは"
+        value={val}
+        onChange={(e) => setVal(e.target.value)} />
+      </div>
+      <h3>{val}</h3>
+      <button onClick={clearVal}>クリア</button>
+    </div>
   );
 };
 
